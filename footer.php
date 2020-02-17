d70eecf6c3de6f5737dabbb604feb1e1bf838c4d
Saposhnikov <jean.saposhnikov@hotmail.fr>
2020-02-17 11:40:41 -0500

    pied de page finalisé

--- footer.php
+++ footer.php
@@ -20,11 +20,11 @@
 		<div class="site-info">
 			<?php $blog_info = get_bloginfo( 'name' ); ?>
 			<?php if ( ! empty( $blog_info ) ) : ?>
-				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>,
+				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 			<?php endif; ?>
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentynineteen' ) ); ?>" class="imprint">
-				
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentynineteen') ); ?>" class="imprint"> | http://localhost/2020-veille/
 			</a>
+			
 			<?php
 			if ( function_exists( 'the_privacy_policy_link' ) ) {
 				the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
